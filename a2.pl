bfea2cea96cfe055516978a2a324282931229278
Hideaki Murakami <thehiderofkeys@gmail.com>
2019-08-27 23:06:17 +1200

    Re wrote a2.pl to work with return value passing

--- a2.pl
+++ a2.pl
@@ -8,32 +8,30 @@
 /** <examples> Your example queries go here, e.g.
 ?- member(X, [cat, mouse]).
 */
-road(A,B,Cost,Network) :-
-    A == B,
-    Cost == 0,
-    Network = Network.
+road(A,A,0,_).
 road(A,B,Cost,Network) :-
 	member((A,X),Network),
 	member((B,Cost),X).
 complete(Path,Network) :-
+	not((member((_,Roads),Network),member((B,_Cost),Roads),
+    not(member(B,Path)))),
 	not((member((A,_),Network),
     not(member(A,Path)))).
 nonrepeating(Path) :-
     is_set(Path).
-middle(Middle,Path) :-
-    Path = [_|End],
-    append(Middle,[_],End).
 cyclic(Path) :-
-    middle(Middle,Path),
-    nonrepeating(Middle).
-solution([StartCity],RoadNetwork,SolutionCost,SolutionPath) :-
-    complete(SolutionPath,RoadNetwork),
-    cyclic(SolutionPath),
-    recursiveDFS(StartCity,RoadNetwork,SolutionCost,SolutionPath).
-recursiveDFS(StartCity,RoadNetwork,0,[StartCity]).
-recursiveDFS(StartCity,RoadNetwork,SolutionCost,SolutionPath) :-
-    append(PrevSolutionPath,[CurrentNode],SolutionPath),
-    append(_,[PreviousNode],PrevSolutionPath),
-    road(PreviousNode,CurrentNode,Cost,RoadNetwork),
-    PreviousSolutionCost == SolutionCost-Cost,
-    recursiveDFS(StartCity,RoadNetwork,PreviousSolutionCost,PrevSolutionPath).
+    append([First],End,Path),
+    append(_,[First],End),
+    nonrepeating(End).
+solution(Path,RoadNetwork,SolutionCost,SolutionPath) :-
+    recursiveDFS(Path,RoadNetwork,0,SolutionCost,SolutionPath).
+recursiveDFS(Path,RoadNetwork,CurrentCost,CurrentCost,Path):-
+    cyclic(Path),
+    complete(Path,RoadNetwork).
+recursiveDFS(Path,RoadNetwork,CurrentCost,SolutionCost,SolutionPath) :-
+    append(_,[CurrentNode],Path),
+    nonrepeating(Path),
+    road(CurrentNode,NextNode,Cost,RoadNetwork),
+    NextCost is CurrentCost+Cost,
+    append(Path,[NextNode],NewPath),
+    recursiveDFS(NewPath,RoadNetwork,NextCost,SolutionCost,SolutionPath).
