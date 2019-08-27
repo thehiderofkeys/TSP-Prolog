8f7d1f185fe62cccec164b76c1cb59d7ed6f6938
Hideaki Murakami <thehiderofkeys@gmail.com>
2019-08-27 17:58:17 +1200

    Addded existing work

---
+++ a2.pl
@@ -0,0 +1,38 @@
+% Student exercise profile
+:- set_prolog_flag(occurs_check, error).        % disallow cyclic terms
+:- set_prolog_stack(global, limit(8 000 000)).  % limit term space (8Mb)
+:- set_prolog_stack(local,  limit(2 000 000)).  % limit environment space
+
+% Your program goes here
+                    
+/** <examples> Your example queries go here, e.g.
+?- member(X, [cat, mouse]).
+*/
+road(A,B,Cost,Network) :-
+    A == B,
+    Cost = 0,
+    Network = Network.
+road(A,B,Cost,Network) :-
+	member((A,X),Network),
+	member((B,Cost),X).
+complete(Path,Network) :-
+	not((member((A,_),Network),
+    not(member(A,Path)))).
+nonrepeating(Path) :-
+    is_set(Path).
+middle(Middle,Path) :-
+    Path = [_|End],
+    append(Middle,[_],End).
+cyclic(Path) :-
+    middle(Middle,Path),
+    nonrepeating(Middle).
+solution([StartCity],RoadNetwork,SolutionCost,SolutionPath) :-
+    complete(SolutionPath,RoadNetwork),
+    cyclic(SolutionPath),
+    recursiveDFS(StartCity,RoadNetwork,SolutionCost,SolutionPath).
+recursiveDFS(StartCity,RoadNetwork,0,[StartCity]).
+recursiveDFS(StartCity,RoadNetwork,SolutionCost,SolutionPath) :-
+    append(PrevSolutionPath,[PreviousNode,CurrentNode],SolutionPath),
+    road(PreviousNode,CurrentNode,Cost,RoadNetwork),
+    PreviousSolutionCost = SolutionCost-Cost,
+    recursiveDFS(StartCity,RoadNetwork,PreviousSolutionCost,PrevSolutionPath).
